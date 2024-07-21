e2d3c5b9ff3fbfce30db584ff765607d0707c35a
Saleh Kaddoura <salehkaddoura@gmail.com>
2024-07-20 19:27:02 -0700

    hosted on firebase

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ Hello Cedar Eng Team,
 
 My Name is Saleh Kaddoura and this is my submission of the Cedar Front end tech challenge.
 
+You can view the app hosted publicly here https://cedar-ad46f.web.app/
+
 - To view the Payment Flow locally please clone the repo and then run `npm install` 
 - `npm run dev` will launch the app locally on `http://localhost:5173/`
 - To run test please use the `npm run test` command
+
+One thing to note, I did not have any time to mock out any backend API calls. If I did have some more time
+I would have added some JSON responses and returned them to my components and passed them down appropriatley.
+
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4u5gTEetrtXPj__1Ut9x080mFNxULq64",
+  authDomain: "cedar-ad46f.firebaseapp.com",
+  projectId: "cedar-ad46f",
+  storageBucket: "cedar-ad46f.appspot.com",
+  messagingSenderId: "822448731142",
+  appId: "1:822448731142:web:2396c5a94df9a9485f3f87"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
